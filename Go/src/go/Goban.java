028a26c45c8bbac6c9ed75dc1a616916ddafeeba
alexis <adumas37@outlook.com>
2014-12-15 09:50:19 +0100

    Modifications pour passer d'une array list a un tableau pour la liste des pierres

--- Go/src/go/Goban.java
+++ Go/src/go/Goban.java
@@ -14,23 +14,19 @@ public class Goban {
 
     private int height;
     private int width;
-    private ArrayList<Pierre> listePierres;
+    private Pierre[][] listePierres;
 
-
-    
-    
-
-    public Goban(int height, int width, ArrayList<Pierre> listePierres) {
+    public Goban(int height, int width, Pierre[][] listePierres) {
         this.height = height;
         this.width = width;
         this.listePierres = listePierres;
     }
 
-    public ArrayList<Pierre> getListePierres() {
+    public Pierre[][] getListePierres() {
         return listePierres;
     }
 
-    public void setListePierres(ArrayList<Pierre> listePierres) {
+    public void setListePierres(Pierre[][] listePierres) {
         this.listePierres = listePierres;
     }
 
@@ -42,22 +38,49 @@ public class Goban {
         return width;
     }
 
-        public void setHeight(int height) {
+    public void setHeight(int height) {
         this.height = height;
     }
 
     public void setWidth(int width) {
         this.width = width;
     }
-    
 
     public boolean intersectionLibre(Point2D p) {
         boolean estLibre = true;
-        for (Pierre pi : listePierres) {
-            if (pi.getPosition().equals(p)) {
-                return false;
-            }
+        if (listePierres[p.getX()][p.getY()]!=null){
+            estLibre = false;
         }
         return estLibre;
+    }
+
+    public boolean horsPlateau(Point2D p) {
+        if ((p.getX() < 0) || (p.getX() >= width) || (p.getY() < 0) || (p.getY() >= height)) {
+            return false;
+        }
+        return true;
+    }
+
+    public int nombreLibertes(Pierre pi) {
+        int lib = 4;
+        ArrayList<Point2D> listeAdjacents = new ArrayList<>();
+        Point2D nord = new Point2D (pi.getPosition().getX(), pi.getPosition().getY()+1);
+        Point2D sud = new Point2D (pi.getPosition().getX(), pi.getPosition().getY()-1);
+        Point2D est = new Point2D (pi.getPosition().getX()+1, pi.getPosition().getY());
+        Point2D ouest = new Point2D (pi.getPosition().getX()-1, pi.getPosition().getY());
+        listeAdjacents.add(nord);
+        listeAdjacents.add(sud);
+        listeAdjacents.add(est);
+        listeAdjacents.add(ouest);
+        
+        for (Point2D p : listeAdjacents) {
+            if (horsPlateau(p) || !intersectionLibre(p))
+            {
+                lib--;
+            }
+        }
+
+
+        return lib;
     }
 }
