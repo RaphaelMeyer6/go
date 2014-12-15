33b1f73a4d94f272ddd8af6ee746bd4bc54a4d09
alexis <adumas37@outlook.com>
2014-12-15 08:39:01 +0100

    Implementation des constructeurs et accesseurs de la classe Pierre

--- go/src/go/Pierre.java
+++ go/src/go/Pierre.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package go;
 
 /**
@@ -11,4 +6,42 @@ package go;
  */
 public class Pierre {
     
+   boolean blanc;
+   Point2D position;
+   
+   /**
+    * Constructeur par défaut
+    * @param blanc
+    * @param position 
+    */
+   public Pierre(boolean blanc, Point2D position){
+       this.blanc=blanc;
+       this.position=new Point2D(position);
+   }
+   
+   /**
+    * Constructeur pour une position x,y
+    * @param blanc
+    * @param x
+    * @param y 
+    */
+   public Pierre(boolean blanc,int x,int y){
+       this.blanc=blanc;
+       this.position= new Point2D(x,y);
+   }
+   /**
+    * Return true if white
+    * @return 
+    */
+   public boolean isBlanc(){
+       return this.blanc;
+   }
+   /**
+    * Return the position
+    * @return 
+    */
+   public Point2D getPosition(){
+       return this.position;
+   }
+           
 }
