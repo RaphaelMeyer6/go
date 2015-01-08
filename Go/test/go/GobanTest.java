1c2e080c53f463ae28cf256e0ce152f4c73509a0
Erwan <Erwan@ext-130-66-204-170-wifi.ec-nantes.fr>
2015-01-08 08:40:54 +0100

    Test for GobanTest testIntersectionLibre

--- Go/test/go/GobanTest.java
+++ Go/test/go/GobanTest.java
@@ -39,153 +39,26 @@ public class GobanTest {
     }
 
     /**
-     * Test of getListePierres method, of class Goban.
-     */
-    @Test
-    public void testGetListePierres() {
-        System.out.println("getListePierres");
-        Goban instance = null;
-        Pierre[][] expResult = null;
-        Pierre[][] result = instance.getListePierres();
-        assertArrayEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setListePierres method, of class Goban.
-     */
-    @Test
-    public void testSetListePierres() {
-        System.out.println("setListePierres");
-        Pierre[][] listePierres = null;
-        Goban instance = null;
-        instance.setListePierres(listePierres);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getHeight method, of class Goban.
-     */
-    @Test
-    public void testGetHeight() {
-        System.out.println("getHeight");
-        Goban instance = null;
-        int expResult = 0;
-        int result = instance.getHeight();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getBlanchesCapturees method, of class Goban.
-     */
-    @Test
-    public void testGetBlanchesCapturees() {
-        System.out.println("getBlanchesCapturees");
-        Goban instance = null;
-        int expResult = 0;
-        int result = instance.getBlanchesCapturees();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setBlanchesCapturees method, of class Goban.
-     */
-    @Test
-    public void testSetBlanchesCapturees() {
-        System.out.println("setBlanchesCapturees");
-        int blanchesCapturees = 0;
-        Goban instance = null;
-        instance.setBlanchesCapturees(blanchesCapturees);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getNoiresCapturees method, of class Goban.
-     */
-    @Test
-    public void testGetNoiresCapturees() {
-        System.out.println("getNoiresCapturees");
-        Goban instance = null;
-        int expResult = 0;
-        int result = instance.getNoiresCapturees();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setNoiresCapturees method, of class Goban.
-     */
-    @Test
-    public void testSetNoiresCapturees() {
-        System.out.println("setNoiresCapturees");
-        int noiresCapturees = 0;
-        Goban instance = null;
-        instance.setNoiresCapturees(noiresCapturees);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getWidth method, of class Goban.
-     */
-    @Test
-    public void testGetWidth() {
-        System.out.println("getWidth");
-        Goban instance = null;
-        int expResult = 0;
-        int result = instance.getWidth();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setHeight method, of class Goban.
-     */
-    @Test
-    public void testSetHeight() {
-        System.out.println("setHeight");
-        int height = 0;
-        Goban instance = null;
-        instance.setHeight(height);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setWidth method, of class Goban.
-     */
-    @Test
-    public void testSetWidth() {
-        System.out.println("setWidth");
-        int width = 0;
-        Goban instance = null;
-        instance.setWidth(width);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
      * Test of intersectionLibre method, of class Goban.
      */
     @Test
     public void testIntersectionLibre() {
         System.out.println("intersectionLibre");
-        Point2D p = null;
-        Goban instance = null;
+        Point2D p = new Point2D(2,2);
+        Point2D p2 = new Point2D(1,1);
+        Goban instance = new Goban(5,5);     
+        Pierre[][] listePierres = new Pierre[5][5];
+        listePierres[2][2] = new Pierre(true,p);
+        instance.setListePierres(listePierres);
+        
         boolean expResult = false;
         boolean result = instance.intersectionLibre(p);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        expResult = true;
+        result = instance.intersectionLibre(p2);
+        assertEquals(expResult, result);
+
     }
 
     /**
