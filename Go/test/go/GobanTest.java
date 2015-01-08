8a2bd1805e71c992eb5ce9074003add9f5894329
Erwan <Erwan@ext-130-66-204-170-wifi.ec-nantes.fr>
2015-01-08 11:31:55 +0100

    Test Voisins ajouté

--- Go/test/go/GobanTest.java
+++ Go/test/go/GobanTest.java
@@ -170,27 +170,37 @@ public class GobanTest {
         Point2D p = new Point2D(0,0);
         Point2D p1 = new Point2D(0,1);
         Point2D p2 = new Point2D(1,1);
-        Point2D p3 = new Point2D(1,0);
-        Point2D p5 = new Point2D(2,2);
+        Point2D p3 = new Point2D(0,2);
+        Point2D p4 = new Point2D(3,3);
         Goban instance = new Goban(5,5);
         Pierre[][] listePierres = new Pierre[5][5];
         Pierre pi = new Pierre(true,p);
         Pierre pi1 = new Pierre (true,p1);
         Pierre pi2 = new Pierre(false,p2);
         Pierre pi3 = new Pierre (false,p3);
-        Pierre pi5 = new Pierre (false,p5);
+        Pierre pi4 = new Pierre (false,p4);
         listePierres[0][0] = pi;
         listePierres[0][1] = pi1;
         listePierres[1][1] = pi2;
-        listePierres[1][0] = pi3;
-        listePierres[2][2] = pi5;
+        listePierres[0][2] = pi3;
+        listePierres[3][3] = pi4;
         instance.setListePierres(listePierres);
         
         
         
         ArrayList<Pierre> expResult = new ArrayList();
         expResult.add(pi1);
-        ArrayList<Pierre> result = instance.voisins(pi1, true);
+        ArrayList<Pierre> result = instance.voisins(pi, true);
+        assertEquals(expResult, result);
+        
+        expResult.clear();
+        expResult.add(pi3);        
+        expResult.add(pi2);
+        result = instance.voisins(pi1, false);
+        assertEquals(expResult, result);
+        
+        expResult.clear();
+        result = instance.voisins(pi4, true);
         assertEquals(expResult, result);
 
     }
