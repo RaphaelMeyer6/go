08231c6878c52853e6e97c1f6ec069c2ac82ba83
ECN <ECN@130.66.208.247>
2015-01-08 09:01:20 +0100

    Finalisation du fichier de test de la classe Groupe, 100% OK

--- Go/test/go/GroupeTest.java
+++ Go/test/go/GroupeTest.java
@@ -59,22 +59,33 @@ public class GroupeTest {
     @Test
     public void testFusionnerGroupes() {
         System.out.println("fusionnerGroupes");
+        boolean bool=false;
         Point2D p=new Point2D(1,1);
+        Point2D p1=new Point2D(1,2);
         Point2D p2=new Point2D(2,2);
+        Point2D p3=new Point2D(2,1);
         
         ArrayList<Pierre> pierres = new ArrayList<>();
-        Pierre Pi1=new Pierre(true,p);
-        pierres.add(Pi1);
+        Pierre Pi=new Pierre(true,p);
+        Pierre Pi3=new Pierre(true,p3);
+        pierres.add(Pi);
+        pierres.add(Pi3);
         Groupe instance = new Groupe(pierres);
         
         ArrayList<Pierre> pierres2 = new ArrayList<>();
-        Pierre Pi2=new Pierre(true,p);
+        Pierre Pi1=new Pierre(true,p1);
+        Pierre Pi2=new Pierre(true,p2);
+        pierres2.add(Pi1);
         pierres2.add(Pi2);
         Groupe g2 = new Groupe(pierres2);
         
         instance.fusionnerGroupes(g2);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        if(Pi1.getGroupe()==instance && Pi2.getGroupe()==instance){
+            if(instance.getPierres().contains(Pi1) && instance.getPierres().contains(Pi2)){
+                   bool=true;
+            }
+        }
+        assertEquals(bool,true);
     }
     
 }
